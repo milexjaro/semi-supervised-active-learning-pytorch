4aa6ee431559c3c2f1cdf04bf3688cd1866781d7
milexjaro <milexjaro@hotmail.com>
2022-07-13 16:32:17 +0100

    Feat: Adjust DataLoader by removing sampler

--- semi-supervised/datautils.py
+++ semi-supervised/datautils.py
@@ -1,3 +1,4 @@
+from random import shuffle
 import torch
 import numpy as np
 import sys
@@ -44,17 +45,24 @@ class SpriteDataset(Dataset):
 
 class SemiSupervisedActiveLearningDataset(Dataset):
     """Dataset for Semi-Supervised Active Learning experimentation"""
-    def __init__(self, data_location, transform=None, target_transform=None, algorithm="", train=True):
+    def __init__(self, data_location, is_labelled=False, transform=None, target_transform=None, algorithm="", train=True):
         self.transform = transform
         self.target_transform = target_transform
         
         try:
             train_infix = 'train' if train else 'test'
-            algorithm_suffix = f'_{algorithm}' if algorithm else ''
-            data_file_location = f"{data_location}/X_{train_infix}{algorithm_suffix}.pt"
-            targets_file_location = f"{data_location}/y_{train_infix}{algorithm_suffix}.pt"
-            self.data = torch.from_numpy(torch.load(data_file_location))
-            self.data = self.data.reshape(len(self.data), 28, 28)
+            if train:
+                labelled_infix = '_labelled' if is_labelled else '_unlabelled'
+                algorithm_suffix = f'_{algorithm}' if algorithm else ''
+            else:
+                labelled_infix = ''
+                algorithm_suffix = ''
+            
+            data_file_location = f"{data_location}/X_{train_infix}{labelled_infix}{algorithm_suffix}.pt"
+            targets_file_location = f"{data_location}/y_{train_infix}{labelled_infix}{algorithm_suffix}.pt"
+
+            # Squeeze is needed because it has (x, 1, 28, 28 dimension)
+            self.data = torch.squeeze(torch.from_numpy(torch.load(data_file_location)))
             self.targets = torch.from_numpy(torch.load(targets_file_location))
         except FileNotFoundError:
             print("File is not found")
@@ -94,24 +102,20 @@ class SemiSupervisedActiveLearningDataset(Dataset):
         return self.data
 
 
-def get_mnist(location="./", batch_size=64, labels_per_class=100, is_original = False):
+def get_mnist(location="./", batch_size=64, algorithm=None):
     from functools import reduce
     from operator import __or__
     from torch.utils.data.sampler import SubsetRandomSampler
-    # from torchvision.datasets import MNIST
     import torchvision.transforms as transforms
     from utils import onehot
 
     flatten_bernoulli = lambda x: transforms.ToTensor()(x).view(-1).bernoulli()
 
-    # mnist_train = MNIST(location, train=True, download=True,
-    #                     transform=flatten_bernoulli, target_transform=onehot(n_labels))
-    # mnist_valid = MNIST(location, train=False, download=True,
-    #                     transform=flatten_bernoulli, target_transform=onehot(n_labels))
-
-    mnist_train = SemiSupervisedActiveLearningDataset(f'{location}', train=True,
-                        transform=flatten_bernoulli, target_transform=onehot(n_labels))
-    mnist_valid = SemiSupervisedActiveLearningDataset(f'{location}', train=False,
+    mnist_train_labelled = SemiSupervisedActiveLearningDataset(f'{location}', train=True, is_labelled=True,
+                        transform=flatten_bernoulli, target_transform=onehot(n_labels), algorithm=algorithm)
+    mnist_train_unlabelled = SemiSupervisedActiveLearningDataset(f'{location}', train=True, is_labelled=False,
+                        transform=flatten_bernoulli, target_transform=onehot(n_labels), algorithm=algorithm)
+    mnist_test = SemiSupervisedActiveLearningDataset(f'{location}', train=False,
                         transform=flatten_bernoulli, target_transform=onehot(n_labels))
 
 
@@ -128,19 +132,11 @@ def get_mnist(location="./", batch_size=64, labels_per_class=100, is_original = 
         return sampler
 
     # Dataloaders for MNIST
-    labelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                           sampler=get_sampler(mnist_train.train_labels.numpy(), labels_per_class))
-    unlabelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                             sampler=get_sampler(mnist_train.train_labels.numpy()))
-    validation = torch.utils.data.DataLoader(mnist_valid, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                             sampler=get_sampler(mnist_valid.test_labels.numpy()))
+    labelled = torch.utils.data.DataLoader(mnist_train_labelled, batch_size=batch_size, num_workers=2, pin_memory=cuda,
+                                           shuffle=True)
+    unlabelled = torch.utils.data.DataLoader(mnist_train_unlabelled, batch_size=batch_size, num_workers=2, pin_memory=cuda,
+                                             shuffle=True)
+    test = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, num_workers=2, pin_memory=cuda,
+                                             shuffle=True)
 
-                                             
-    # labelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-    #                                        sampler=get_sampler(mnist_train.train_labels.cpu().numpy(), labels_per_class))
-    # unlabelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-    #                                          sampler=get_sampler(mnist_train.train_labels.cpu().numpy()))
-    # validation = torch.utils.data.DataLoader(mnist_valid, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-    #                                          sampler=get_sampler(mnist_valid.test_labels.cpu().numpy()))
-
-    return labelled, unlabelled, validation
+    return labelled, unlabelled, test
