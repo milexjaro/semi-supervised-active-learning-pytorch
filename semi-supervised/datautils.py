705f3f6e9370e0c60fea673a1426cdd75f66ddac
milexjaro <milexjaro@hotmail.com>
2022-07-15 00:47:02 +0100

    Fix: Label condition

--- semi-supervised/datautils.py
+++ semi-supervised/datautils.py
@@ -65,7 +65,7 @@ class SemiSupervisedActiveLearningDataset(Dataset):
             print(targets_file_location)
 
             # Squeeze is needed because it has (x, 1, 28, 28 dimension)
-            self.data = torch.squeeze(torch.from_numpy(torch.load(data_file_location)))
+            self.data = torch.squeeze(torch.from_numpy(torch.load(data_file_location))) * 255
             self.targets = torch.from_numpy(torch.load(targets_file_location))
             print(len(self.data), len(self.targets))
         except FileNotFoundError:
@@ -137,16 +137,34 @@ def get_mnist(location="./", batch_size=64, labels_per_class=100, algorithm=None
 
     # Dataloaders for MNIST
     labelled = torch.utils.data.DataLoader(mnist_train_labelled, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                           sampler=get_sampler(mnist_train_labelled.train_labels.numpy()))
+                                           sampler=RandomSampler(mnist_train_labelled))
     unlabelled = torch.utils.data.DataLoader(mnist_train_unlabelled, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                           sampler=get_sampler(mnist_train_unlabelled.train_labels.numpy()))
+                                           sampler=RandomSampler(mnist_train_unlabelled))
     test = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                           sampler=get_sampler(mnist_test.train_labels.numpy()))
+                                           sampler=RandomSampler(mnist_test))
 
     return labelled, unlabelled, test
 
+def get_mnist_dataset(location="./", batch_size=64, labels_per_class=100, algorithm=None):
+    from functools import reduce
+    from operator import __or__
+    from torch.utils.data.sampler import SubsetRandomSampler, RandomSampler
+    from torchvision.datasets import MNIST
+    import torchvision.transforms as transforms
+    from utils import onehot
 
-def get_mnist_v2(location="./", batch_size=64, labels_per_class=100, indices = None):
+    flatten_bernoulli = lambda x: transforms.ToTensor()(x).view(-1).bernoulli()
+
+    mnist_train_labelled = SemiSupervisedActiveLearningDataset(f'{location}', train=True, is_labelled=True,
+                        transform=flatten_bernoulli, target_transform=onehot(n_labels), algorithm=algorithm)
+    mnist_train_unlabelled = SemiSupervisedActiveLearningDataset(f'{location}', train=True, is_labelled=False,
+                        transform=flatten_bernoulli, target_transform=onehot(n_labels), algorithm=algorithm)
+    mnist_test = SemiSupervisedActiveLearningDataset(f'{location}', train=False,
+                        transform=flatten_bernoulli, target_transform=onehot(n_labels))
+
+    return mnist_train_labelled, mnist_train_unlabelled, mnist_test
+
+def get_mnist_legacy_dataset(location="./", batch_size=64, labels_per_class=100):
     from functools import reduce
     from operator import __or__
     from torch.utils.data.sampler import SubsetRandomSampler
@@ -158,30 +176,11 @@ def get_mnist_v2(location="./", batch_size=64, labels_per_class=100, indices = N
 
     mnist_train = MNIST(location, train=True, download=True,
                         transform=flatten_bernoulli, target_transform=onehot(n_labels))
-    mnist_test = MNIST(location, train=False, download=True,
+    mnist_valid = MNIST(location, train=False, download=True,
                         transform=flatten_bernoulli, target_transform=onehot(n_labels))
 
-    def get_sampler(labels, indices, n=None):
-        # Only choose digits in n_labels
-        # (indices,) = np.where(reduce(__or__, [labels == i for i in np.arange(n_labels)]))
+    return mnist_train, mnist_valid
 
-        # Ensure uniform distribution of labels
-        np.random.shuffle(indices)
-        indices = np.hstack([list(filter(lambda idx: labels[idx] == i, indices))[:n] for i in range(n_labels)])
-
-        indices = torch.from_numpy(indices)
-        sampler = SubsetRandomSampler(indices)
-        return sampler
-
-    # Dataloaders for MNIST
-    labelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                           sampler=get_sampler(mnist_train.train_labels.numpy(), indices))
-    unlabelled = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                             sampler=get_sampler(mnist_train.train_labels.numpy(), indices))
-    test = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, num_workers=2, pin_memory=cuda,
-                                             sampler=get_sampler(mnist_test.test_labels.numpy(), indices))
-
-    return labelled, unlabelled, test
 
 
 def get_mnist_legacy(location="./", batch_size=64, labels_per_class=100):
